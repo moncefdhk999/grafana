05539aacc7570e0bcb4ea46507a91204fd330cd4
Levente Balogh <balogh.levente.hu@gmail.com>
2024-09-02 10:40:47 +0200

    FIXUP

--- public/app/features/plugins/extensions/ExtensionRegistriesContext.tsx
+++ public/app/features/plugins/extensions/ExtensionRegistriesContext.tsx
@@ -1,23 +1,34 @@
 import { PropsWithChildren, createContext, useContext } from 'react';
 
-import { ReactivePluginExtensionsRegistry } from 'app/features/plugins/extensions/reactivePluginExtensionRegistry';
+import { AddedComponentsRegistry } from 'app/features/plugins/extensions/registry/AddedComponentsRegistry';
+import { AddedLinksRegistry } from 'app/features/plugins/extensions/registry/AddedLinksRegistry';
 import { ExposedComponentsRegistry } from 'app/features/plugins/extensions/registry/ExposedComponentsRegistry';
 
 export interface ExtensionRegistriesContextType {
   registries: {
-    common: ReactivePluginExtensionsRegistry;
+    addedLinks: AddedLinksRegistry;
+    addedComponents: AddedComponentsRegistry;
     exposedComponents: ExposedComponentsRegistry;
   };
 }
 
 // Using a different context for each registry to avoid unnecessary re-renders
-export const ReactiveExtensionRegistryContext = createContext<ReactivePluginExtensionsRegistry | undefined>(undefined);
+export const AddedLinksRegistryContext = createContext<AddedLinksRegistry | undefined>(undefined);
+export const AddedComponentsRegistryContext = createContext<AddedComponentsRegistry | undefined>(undefined);
 export const ExposedComponentsRegistryContext = createContext<ExposedComponentsRegistry | undefined>(undefined);
 
-export function useReactiveExtensionRegistryContext(): ExposedComponentsRegistry {
-  const context = useContext(ExposedComponentsRegistryContext);
+export function useAddedLinksRegistry(): AddedLinksRegistry {
+  const context = useContext(AddedLinksRegistryContext);
   if (!context) {
-    throw new Error('No `ExposedComponentsRegistryContext` found.');
+    throw new Error('No `AddedLinksRegistryContext` found.');
+  }
+  return context;
+}
+
+export function useAddedComponentsRegistry(): AddedComponentsRegistry {
+  const context = useContext(AddedComponentsRegistryContext);
+  if (!context) {
+    throw new Error('No `AddedComponentsRegistryContext` found.');
   }
   return context;
 }
@@ -35,10 +46,12 @@ export const ExtensionRegistriesProvider = ({
   children,
 }: PropsWithChildren<ExtensionRegistriesContextType>) => {
   return (
-    <ExposedComponentsRegistryContext.Provider value={registries.exposedComponents}>
-      <ReactiveExtensionRegistryContext.Provider value={registries.common}>
-        {children}
-      </ReactiveExtensionRegistryContext.Provider>
-    </ExposedComponentsRegistryContext.Provider>
+    <AddedLinksRegistryContext.Provider value={registries.addedLinks}>
+      <AddedComponentsRegistryContext.Provider value={registries.addedComponents}>
+        <ExposedComponentsRegistryContext.Provider value={registries.exposedComponents}>
+          {children}
+        </ExposedComponentsRegistryContext.Provider>
+      </AddedComponentsRegistryContext.Provider>
+    </AddedLinksRegistryContext.Provider>
   );
 };
